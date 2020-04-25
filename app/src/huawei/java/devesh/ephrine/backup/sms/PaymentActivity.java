470fb9cd2c12973b996c47e16504c03e9cce8bf9
Devesh Chaudhari <DeveshRx@users.noreply.github.com>
2020-04-25 21:17:39 +0530

    VersionCode 25
    Beta Version: 0.0.5
    * Dark Mode Added
    * Bugs fixes
    * Battery optimization
    * Improved UI

---
+++ app/src/huawei/java/devesh/ephrine/backup/sms/PaymentActivity.java
@@ -0,0 +1,14 @@
+package devesh.ephrine.backup.sms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_samsung_paymeny);
+    }
+}
