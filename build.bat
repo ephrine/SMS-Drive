ec45f036e3648692ae1a4cb53fd34d645aa72a8e
Devesh Chaudhari <DeveshRx@users.noreply.github.com>
2020-06-01 14:37:23 +0530

    build

--- build.bat
+++ build.bat
@@ -1,6 +1,17 @@
 :: This batch file gradle builds all release apk for production
 ECHO OFF
+set message====================== Welcome to SMS Drive ===================
+set message0= Product of Ephrine Apps (C) 2020 
+set message1= Developed by Devesh Chaudhari (C) 2020
+set message2= Building Debug APK .....
 
-gradlew tasks assembleGalaxy && gradlew tasks assembleMaster
+echo %message%
+echo %message0%
+echo %message1%
+echo %message2%
+gradlew tasks assembleGalaxy --stacktrace && gradlew tasks assembleMaster --stacktrace
+
+set messageend======================Welcome to SMS Drive Debug APK has Generated===================  
+echo %messageend%
 
 PAUSE
--- build_debug.bat
+++ build_debug.bat
@@ -1,6 +1,13 @@
 :: This batch file gradle builds all release apk for production
 ECHO OFF
 
+set message======================Welcome to SMS Drive Build===================  
+echo %message%
+
 gradlew tasks assembleGalaxy --stacktrace && gradlew tasks assembleMaster --stacktrace
+
+
+set messageend======================Welcome to SMS Drive Debug APK has Generated===================  
+echo %messageend%
 
 PAUSE
